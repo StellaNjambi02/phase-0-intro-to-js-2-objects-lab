ba287e938cf2995e6fd9d66ddd9201e90d2a3964
StellaNjambi02 <njambindungistella@gmail.com>
2024-06-28 14:54:07 +0300

    objects lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee={
+    name:"Stella",
+    streetAddress:"Mudumo",
+}
+
+function updateEmployeeWithKeyAndValue(employee,key,value){
+
+    const newEmployee={...employee};
+    newEmployee[key]="11 Broadway";
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+    employee[key]="12 Broadway";
+    return employee;
+    
+}
+function deleteFromEmployeeByKey(employee,key){
+
+    const newEmployee={...employee};
+    delete newEmployee[key];
+    return newEmployee;
+}
+ function destructivelyDeleteFromEmployeeByKey(employee,key){
+delete employee[key];
+    return employee;
+ }
